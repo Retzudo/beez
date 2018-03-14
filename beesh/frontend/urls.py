2c6b9f92cf37044ff88f6350431c3e01e938ebc5
Retzudo <christoph.willinger@gmail.com>
2018-03-14 09:59:32 +0100

    Views

--- beesh/frontend/urls.py
+++ beesh/frontend/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from frontend import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', TemplateView.as_view(template_name='frontend/index.html'), name='index'),
+    path('apiaries', views.ApiaryListView.as_view(), name='apiary-list'),
+    path('apiaries/<int:pk>', views.ApiaryDetailView.as_view(), name='apiary-detail'),
 ]
--- beesh/frontend/views.py
+++ beesh/frontend/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView
 
 
-def index(request):
-    return render(request, 'frontend/index.html')
+class ApiaryListView(LoginRequiredMixin, ListView):
+    template_name = 'frontend/apiary_list.html'
+
+    def get_queryset(self):
+        return self.request.user.apiaries.all()
+
+
+class ApiaryDetailView(LoginRequiredMixin, DetailView):
+    template_name = 'frontend/apiary_detail.html'
+    context_object_name = 'apiary'
+
+    def get_queryset(self):
+        return self.request.user.apiaries.all()
