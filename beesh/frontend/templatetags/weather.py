d71753ebafa0e1030ed64af47badf32b0a10adb6
Retzudo <christoph.willinger@gmail.com>
2018-03-16 10:53:06 +0100

    UX

---
+++ beesh/frontend/templatetags/weather.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from django import template
+
+register = template.Library()
+
+icon_map = {
+    'Rain': 'day-rain',
+    'Snow': 'day-snow',
+}
+
+
+@register.filter
+def weather_icon(condition):
+    return icon_map.get(condition, 'day-sunny')
