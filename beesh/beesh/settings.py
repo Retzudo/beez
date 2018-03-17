c9245f83fbdc1cff70e650dac73e2a44fed75fc6
Retzudo <christoph.willinger@gmail.com>
2018-03-17 14:41:24 +0100

    static files

--- beesh/beesh/settings.py
+++ beesh/beesh/settings.py
@@ -105,6 +105,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 
 OWM_API_KEY = os.getenv('BEESH_OWM_API_KEY')
 LOGIN_REDIRECT_URL = '/dashboard'
