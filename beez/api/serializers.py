9fb227e46e1ef66a5d3ff603b617cc593ca9fc16
Retzudo <christoph.willinger@gmail.com>
2018-05-15 09:59:10 +0200

    Queen API

--- beez/api/serializers.py
+++ beez/api/serializers.py
@@ -16,11 +16,42 @@ class QueenSerializer(serializers.ModelSerializer):
 
 
 class HiveSerializer(serializers.ModelSerializer):
-    queen = QueenSerializer()
+    queen = QueenSerializer(required=False, allow_null=True)
 
     class Meta:
         model = models.Hive
         fields = '__all__'
+
+    def create(self, validated_data):
+        queen_data = validated_data.pop('queen')
+        hive = super().create(validated_data)
+
+        models.Queen.objects.create(hive=hive, **queen_data)
+
+        return hive
+
+    def update(self, hive, validated_data):
+        if 'queen' not in validated_data or validated_data['queen'] is None:
+            queen = None
+            try:
+                hive.queen.delete()
+            except models.Queen.DoesNotExist:
+                pass
+        else:
+            queen_data = validated_data.pop('queen')
+
+            try:
+                queen = hive.queen
+            except models.Queen.DoesNotExist:
+                queen = models.Queen(hive=hive)
+
+            queen.year = queen_data['year']
+            queen.number = queen_data['number']
+            queen.save()
+
+        hive.queen = queen
+
+        return super().update(hive, validated_data)
 
 
 class InspectionSerializer(serializers.ModelSerializer):
