17325b8a8a3d56bb75fa80a03d8967ae6662191d
Retzudo <christoph.willinger@gmail.com>
2018-05-28 09:58:58 +0200

    Varroa chart

--- beez/api/filters.py
+++ beez/api/filters.py
@@ -5,6 +5,7 @@ from core import models
 
 class InspectionFilter(filters.FilterSet):
     has_weight = filters.BooleanFilter(field_name='weight', lookup_expr='isnull', exclude=True)
+    has_mites = filters.BooleanFilter(field_name='mites_counted', lookup_expr='isnull', exclude=True)
 
     class Meta:
         model = models.Inspection
