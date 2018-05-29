705811df96828e9a82c86bf0cc7c3eda58a64ffb
Retzudo <christoph.willinger@gmail.com>
2018-05-29 10:05:07 +0200

    Varroa treatments in chart

--- beez/api/filters.py
+++ beez/api/filters.py
@@ -6,6 +6,7 @@ from core import models
 class InspectionFilter(filters.FilterSet):
     has_weight = filters.BooleanFilter(field_name='weight', lookup_expr='isnull', exclude=True)
     has_mites = filters.BooleanFilter(field_name='mites_counted', lookup_expr='isnull', exclude=True)
+    has_treatment = filters.BooleanFilter(field_name='mite_treatment', lookup_expr='isnull', exclude=True)
 
     class Meta:
         model = models.Inspection
